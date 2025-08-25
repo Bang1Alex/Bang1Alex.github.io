91e5f7572089fdf2608f2644c50877bebee211c3
Alex Bang <alex184893723@163.com>
2025-08-25 17:19:13 +0800

    remove lock.json

--- site.config.ts
+++ site.config.ts
@@ -1,7 +1,7 @@
 import { defineSiteConfig } from 'valaxy'
 
 export default defineSiteConfig({
-  url: 'https://valaxy.site/',
+  url: 'https://alexbang.netlify.app/',
   lang: 'zh-CN',
   title: 'Alex Notes',
   timezone: 'Asia/Shanghai',
